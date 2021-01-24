23e45741a4b8681ce357ecfd478b5425e5836fe8
Andrea Guffi <andreaguffi92@gmail.com>
2021-01-24 16:04:43 +0100

    shop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ShopPageComponent } from './components/shop-page/shop-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'shop',
+    component: ShopPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce-project';
+
+  onActivate(event) {
+    window.scroll(0, 0);
+  }
 }

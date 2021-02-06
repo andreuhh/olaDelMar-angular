7305e34a02266214a1afd95fcf80289b208496de
Andrea Guffi <andreaguffi92@gmail.com>
2021-02-06 14:54:13 +0100

    ok

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../service/crud.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(
+    public crudService: CrudService
+  ) {
+
+  }
 
   ngOnInit(): void {
+    this.crudService.getItems().subscribe(allProducts => {
+      console.log(allProducts);
+      this.products = allProducts;
+    });
   }
 
 }
